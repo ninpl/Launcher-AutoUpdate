79f2c3604b778e68aa0292eaf5402937d6bf3450
lPinchol <pinchodev91@gmail.com>
2015-03-03 17:19:57 +0100

    Add interface :octocat:
    Add interface and link

--- Launcher/SharpUpdate/LauncherAplicacion/Form1.cs
+++ Launcher/SharpUpdate/LauncherAplicacion/Form1.cs
@@ -5,13 +5,19 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharpUpdate;
 
 namespace LauncherAplicacion
 {
-    public partial class LauncherApp : Form
+    public partial class LauncherApp : Form ,ISharpUpdate
     {
+        private string nameApp = "Launcher APP";
+        private string idApp = "Launcher APP";
+        private string uriApp = "";
+
         public LauncherApp()
         {
             InitializeComponent();
@@ -20,6 +26,36 @@ namespace LauncherAplicacion
         private void BtnCheckUpdate_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public string ApplicationName
+        {
+            get { return nameApp; }
+        }
+
+        public string ApplicationID
+        {
+            get { return idApp; }
+        }
+
+        public Assembly ApplicationAssembly
+        {
+            get { return Assembly.GetExecutingAssembly(); }
+        }
+
+        public Icon ApplicationIcon
+        {
+            get { return this.Icon; }
+        }
+
+        public Uri UpdateXmlLocation
+        {
+            get { return new Uri(uriApp); }
+        }
+
+        public Form context
+        {
+            get { return this; }
         }
     }
 }
---
+++ Launcher/SharpUpdate/SharpUpdate/ISharpUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace SharpUpdate
+{
+    public interface ISharpUpdate
+    {
+        string ApplicationName { get; }
+        string ApplicationID { get; }
+        Assembly ApplicationAssembly { get; }
+        Icon ApplicationIcon { get; }
+        Uri UpdateXmlLocation { get; }
+        Form context { get; }
+    }
+}
