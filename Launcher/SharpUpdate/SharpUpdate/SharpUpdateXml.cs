86cc402324f74ff12df977b9a72c5ab35d194519
lPinchol <pinchodev91@gmail.com>
2015-03-03 18:42:41 +0100

    Add summary SharpUpdateXml :repeat_one:
    Add summary SharpUpdateXml

--- Launcher/SharpUpdate/SharpUpdate/SharpUpdateXml.cs
+++ Launcher/SharpUpdate/SharpUpdate/SharpUpdateXml.cs
@@ -4,6 +4,9 @@ using System.Xml;
 
 namespace SharpUpdate
 {
+    /// <summary>
+    /// contains update information
+    /// </summary>
     internal class SharpUpdateXml
     {
         private Version version;
@@ -13,36 +16,58 @@ namespace SharpUpdate
         private string description;
         private string launchArgs;
 
+        /// <summary>
+        /// The update version #
+        /// </summary>
         internal Version Version
         {
             get { return this.version; }
         }
 
+        /// <summary>
+        /// The location of the update
+        /// </summary>
         internal Uri Uri
         {
             get { return this.uri; }
         }
 
+        /// <summary>
+        /// The file name of the binary
+        /// for use on local computer
+        /// </summary>
         internal string FileName
         {
             get { return this.fileName; }
         }
 
+        /// <summary>
+        /// The MD5 of the update´s binary
+        /// </summary>
         internal string MD5
         {
             get { return this.md5; }
         }
 
+        /// <summary>
+        /// The update´s description
+        /// </summary>
         internal string Description
         {
             get { return this.description; }
         }
 
+        /// <summary>
+        /// The arguments to pass to the updated application on startup
+        /// </summary>
         internal string LaunchArgs
         {
             get { return this.launchArgs; }
         }
 
+        /// <summary>
+        /// Creates a new SharpUpdateXml object
+        /// </summary>
         internal SharpUpdateXml(Version version, Uri uri, string fileName, string md5, string description, string launchArgs)
         {
             this.version = version;
@@ -53,16 +78,28 @@ namespace SharpUpdate
             this.launchArgs = launchArgs;
         }
 
+        /// <summary>
+        /// Checks if update´s version is newer than the old version
+        /// </summary>
+        /// <param name="version">Application´s current version</param>
+        /// <returns>If the update´s version # os newer</returns>
         internal bool IsNewerThan(Version version)
         {
             return this.version > version;
         }
 
+        /// <summary>
+        /// Cheks the Uri to make sure file exist
+        /// </summary>
+        /// <param name="location">The Uri of the update.xml</param>
+        /// <returns>If the file exist</returns>
         internal static bool ExistsOnServer(Uri location)
         {
             try
             {
+                //  Request the update.xml
                 HttpWebRequest req = (HttpWebRequest)WebRequest.Create(location.AbsoluteUri);
+                //  Read for response
                 HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
                 resp.Close();
 
@@ -71,6 +108,12 @@ namespace SharpUpdate
             catch { return false; }
         }
 
+        /// <summary>
+        /// Parses the update.xml into SharpUpdateXml object
+        /// </summary>
+        /// <param name="location">Uri of update.xml on server</param>
+        /// <param name="appID">The application´s ID</param>
+        /// <returns>The SharpUpdateXml object with the data or null of any errores</returns>
         internal static SharpUpdateXml Parse(Uri location, string appID)
         {
             Version version = null;
@@ -78,16 +121,21 @@ namespace SharpUpdate
 
             try
             {
+                //  Load the document
                 XmlDocument doc = new XmlDocument();
                 doc.Load(location.AbsoluteUri);
 
+                //  Gets the appId node with the update info
+                //  This allows you to store all programs update nodes in one file
                 XmlNode node = doc.DocumentElement.SelectSingleNode("//update[@appID=´" + appID + ";]");
 
+                //  If the node doesnt exist ,there is no update
                 if (node == null)
                 {
                     return null;
                 }
 
+                //  Parse data
                 version = Version.Parse(node["latestVersion"].InnerText);
                 url = node["latestVersionUrl"].InnerText;
                 fileName = node["fileName"].InnerText;
