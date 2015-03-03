9265e968b95dd52b30e9bfaa31ccde9cfa736c37
lPinchol <pinchodev91@gmail.com>
2015-03-03 16:07:21 +0100

    Update Launcher :arrows_counterclockwise:
    Update Launcher base

---
+++ Launcher/SharpUpdate/LauncherAplicacion/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace LauncherAplicacion
+{
+    partial class LauncherApp
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCheckUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnCheckUpdate
+            // 
+            this.BtnCheckUpdate.Location = new System.Drawing.Point(111, 331);
+            this.BtnCheckUpdate.Name = "BtnCheckUpdate";
+            this.BtnCheckUpdate.Size = new System.Drawing.Size(187, 58);
+            this.BtnCheckUpdate.TabIndex = 0;
+            this.BtnCheckUpdate.Text = "Check for Updates";
+            this.BtnCheckUpdate.UseVisualStyleBackColor = true;
+            this.BtnCheckUpdate.Click += new System.EventHandler(this.BtnCheckUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // LauncherApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 491);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCheckUpdate);
+            this.Name = "LauncherApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCheckUpdate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Launcher/SharpUpdate/LauncherAplicacion/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LauncherAplicacion
+{
+    public partial class LauncherApp : Form
+    {
+        public LauncherApp()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheckUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
