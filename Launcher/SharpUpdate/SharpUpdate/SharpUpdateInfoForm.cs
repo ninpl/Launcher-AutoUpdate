2c8c71d02437de3310e3b047024436afb5caa36b
lPinchol <pinchodev91@gmail.com>
2015-03-03 22:09:47 +0100

    Add Form Update :octocat:
    Add Form update info

---
+++ Launcher/SharpUpdate/SharpUpdate/SharpUpdateInfoForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SharpUpdate
+{
+    public partial class SharpUpdateInfoForm : Form
+    {
+        public SharpUpdateInfoForm(ISharpUpdate applicationInfo,SharpUpdateXml updateInfo)
+        {
+            InitializeComponent();
+
+            if (applicationInfo.ApplicationIcon != null)
+            {
+                this.Icon = applicationInfo.ApplicationIcon;
+            }
+
+            this.Text = applicationInfo.ApplicationName + " . Update Info";
+            this.lblVersions.Text = string.Format("Current Version: {0}\nUpdate Version: {1}",applicationInfo.ApplicationAssembly.GetName().Version.ToString()),
+                updateInfo.Version.ToString();
+            this.txtDescription.Text = updateInfo.Description;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtDescription_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!(e.Control && e.KeyCode == Keys.C))
+            {
+                e.SuppressKeyPress = true;
+            }
+        }
+    }
+}
