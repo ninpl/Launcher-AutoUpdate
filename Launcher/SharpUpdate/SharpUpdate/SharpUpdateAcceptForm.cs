27513c659e310e7a83f6818700944f50a26bdaec
lPinchol <pinchodev91@gmail.com>
2015-03-04 03:59:31 +0100

    Create SharpUpdateAcceptForm :octopus:
    Create base SharpUpdateAcceptForm and summary

---
+++ Launcher/SharpUpdate/SharpUpdate/SharpUpdateAcceptForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SharpUpdate
+{
+    /// <summary>
+    /// Form to prompt the user to accept the update
+    /// </summary>
+    internal partial class SharpUpdateAcceptForm : Form
+    {
+        /// <summary>
+        /// The program to updates info
+        /// </summary>
+        private ISharpUpdate applicationInfo;
+
+        /// <summary>
+        /// The update info from the update.xml
+        /// </summary>
+        private SharpUpdateXml updateInfo;
+
+        /// <summary>
+        /// The update info display form
+        /// </summary>
+        private SharpUpdateInfoForm updateInfoForm;
+
+        /// <summary>
+        /// Creates a new SharpUpdateAcceptForm
+        /// </summary>
+        public SharpUpdateAcceptForm(ISharpUpdate applicationInfo,SharpUpdateXml updateInfo)
+        {
+            InitializeComponent();
+
+            this.applicationInfo = applicationInfo;
+            this.updateInfo = updateInfo;
+
+            this.Text = this.applicationInfo.ApplicationName + " . Update Available";
+
+            if (this.applicationInfo.ApplicationIcon != null)
+            {
+                this.Icon = this.applicationInfo.ApplicationIcon;
+            }
+
+            this.lblNewVersion.Text = string.Format("New Version: {0}", this.updateInfo.Version.ToString());
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Yes;
+            this.Close();
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+            this.Close();
+        }
+
+        private void btnDetails_Click(object sender, EventArgs e)
+        {
+            if (this.updateInfoForm == null)
+            {
+                this.updateInfoForm = new SharpUpdateInfoForm(this.applicationInfo, this.updateInfo);
+            }
+
+            this.updateInfoForm.ShowDialog(this);
+        }
+    }
+}
